e9094fb00b03cc08d5df161646d78e4e31752ee1
Alex Hatzipanis <alex@hatzipanis.com>
2019-01-24 15:36:57 +1300

    Update SEO strategy

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -3,11 +3,10 @@ import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
 
 import Header from '../Header'
-import SEOLayout from '../SEO/Layout'
 
 import '../../styles/index.css'
 
-const Layout = ({ children, location }) => (
+const Layout = ({ children }) => (
   <StaticQuery
     query={graphql`
       query SiteTitleQuery {
@@ -20,7 +19,6 @@ const Layout = ({ children, location }) => (
     `}
     render={data => (
       <>
-        <SEOLayout location={location} />
         <div className="font-sans leading-normal">
           <Header siteTitle={data.site.siteMetadata.title} />
           <div className="max-w-xl mx-auto px-4 mt-16 mb-20">{children}</div>
@@ -31,7 +29,6 @@ const Layout = ({ children, location }) => (
 )
 
 Layout.propTypes = {
-  location: PropTypes.object.isRequired,
   children: PropTypes.node.isRequired,
 }
 
