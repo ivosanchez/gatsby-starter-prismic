c08b3f3b244fd1deaced4d8619e0c1e7eaea4d36
Alex Hatzipanis <alex@hatzipanis.com>
2019-01-24 15:34:24 +1300

    Remove purgecss-webpack-plugin logic

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,32 +1,8 @@
 const chalk = require('chalk')
-const PurgeCssPlugin = require('purgecss-webpack-plugin')
 const path = require('path')
-const glob = require('glob')
 const { paginate } = require('gatsby-awesome-pagination')
 
 const log = console.log
-
-const PATHS = {
-  src: path.join(__dirname, 'src'),
-}
-
-const purgeCssConfig = {
-  paths: glob.sync(`${PATHS.src}/**/*.js`, { nodir: true }),
-  extractors: [
-    {
-      extractor: class {
-        static extract(content) {
-          return content.match(/[A-Za-z0-9-_:/]+/g) || []
-        }
-      },
-      extensions: ['js'],
-    },
-  ],
-  // Adjust for each project
-  whitelist: ['class-to-whitelist'],
-  // Adjust for each project
-  whitelistPatterns: [/body/, /headroom/, /ReactModal/, /ril/],
-}
 
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
@@ -245,13 +221,4 @@ exports.onCreateWebpackConfig = ({ actions, stage }) => {
     })
     return true
   }
-
-  // Add PurgeCSS in production
-  // See: https://github.com/gatsbyjs/gatsby/issues/5778#issuecomment-402481270
-  if (stage.includes('build')) {
-    actions.setWebpackConfig({
-      plugins: [new PurgeCssPlugin(purgeCssConfig)],
-    })
-  }
 }
-/* END */
