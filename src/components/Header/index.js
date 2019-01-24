1eb1dd4a89663380b7b256a9742f90491e40332e
Alex Hatzipanis <alex@hatzipanis.com>
2019-01-24 15:34:47 +1300

    Rename favicon

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,7 +8,7 @@ const Header = ({ siteTitle }) => (
   <StaticQuery
     query={graphql`
       query {
-        logo: file(relativePath: { eq: "gatsby-icon.png" }) {
+        logo: file(relativePath: { eq: "icon.png" }) {
           childImageSharp {
             fixed(width: 50) {
               ...GatsbyImageSharpFixed
